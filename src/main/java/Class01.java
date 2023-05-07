eb47a55af87dd455b9477303d66535a3151d495f
Sedat Özveren <sedatozveren@gmail.om>
2023-05-07 23:33:36 +0300

    Version2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,14 +1,16 @@
 public class Class01 {
     public static void main(String[] args) {
-        int x=1;
-        int y=2;
-        System.out.println(x+y);
+        int x = 1;
+        int y = 2;
+        System.out.println(x + y);
         System.out.println("Version 1 için işlemler tamam!");
+        //Version 2 calışmalari
+        int z=4;
+        System.out.println(x+y+z);
 
 
 
     }
-
 
 
 }
